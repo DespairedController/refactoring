a9ab39241eaaee426342fee28403a3c9b1dd78ea
DespairedController <ghjnhesm@gmail.com>
2021-11-28 18:02:26 +0300

    duplicates in QueryServlet removed

--- src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -8,105 +8,75 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.function.BiConsumer;
 
 /**
  * @author akirakozov
  */
 public class QueryServlet extends HttpServlet {
+
+    public static final String DB = "jdbc:sqlite:test.db";
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String command = request.getParameter("command");
 
         if ("max".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
-                    response.getWriter().println("<html><body>");
-                    response.getWriter().println("<h1>Product with max price: </h1>");
-
-                    while (rs.next()) {
-                        String  name = rs.getString("name");
-                        int price  = rs.getInt("price");
-                        response.getWriter().println(name + "\t" + price + "</br>");
-                    }
-                    response.getWriter().println("</body></html>");
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            tryRequest(QueryServlet::pricePrinter, response, "SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1", "<h1>Product with max price: </h1>");
         } else if ("min".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
-                    response.getWriter().println("<html><body>");
-                    response.getWriter().println("<h1>Product with min price: </h1>");
-
-                    while (rs.next()) {
-                        String  name = rs.getString("name");
-                        int price  = rs.getInt("price");
-                        response.getWriter().println(name + "\t" + price + "</br>");
-                    }
-                    response.getWriter().println("</body></html>");
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            tryRequest(QueryServlet::pricePrinter, response, "SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1", "<h1>Product with min price: </h1>");
         } else if ("sum".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT SUM(price) FROM PRODUCT");
-                    response.getWriter().println("<html><body>");
-                    response.getWriter().println("Summary price: ");
-
-                    if (rs.next()) {
-                        response.getWriter().println(rs.getInt(1));
-                    }
-                    response.getWriter().println("</body></html>");
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            tryRequest(QueryServlet::singleValuePrinter, response, "SELECT SUM(price) FROM PRODUCT", "Summary price: ");
         } else if ("count".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM PRODUCT");
-                    response.getWriter().println("<html><body>");
-                    response.getWriter().println("Number of products: ");
-
-                    if (rs.next()) {
-                        response.getWriter().println(rs.getInt(1));
-                    }
-                    response.getWriter().println("</body></html>");
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            tryRequest(QueryServlet::singleValuePrinter, response, "SELECT COUNT(*) FROM PRODUCT", "Number of products: ");
         } else {
             response.getWriter().println("Unknown command: " + command);
         }
 
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    private void tryRequest(BiConsumer<ResultSet, HttpServletResponse> function, HttpServletResponse response, String query, String header) {
+        try {
+            try (Connection c = DriverManager.getConnection(DB)) {
+                Statement stmt = c.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                response.getWriter().println("<html><body>");
+                response.getWriter().println(header);
+
+                function.accept(rs, response);
+                response.getWriter().println("</body></html>");
+
+                rs.close();
+                stmt.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void pricePrinter(ResultSet rs, HttpServletResponse response) {
+        while (true) {
+            try {
+                if (!rs.next()) break;
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+                response.getWriter().println(name + "\t" + price + "</br>");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void singleValuePrinter(ResultSet rs, HttpServletResponse response) {
+        try {
+            if (rs.next()) {
+                response.getWriter().println(rs.getInt(1));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
